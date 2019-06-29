46664c1e2eaf62ba666fe2abb5be90c9cf61ddef
Shaul Fridman <shaul.fridman@gmail.com>
2019-06-30 02:44:52 +0300

    Stashed

--- tests/CompileTimeRandomTest.cpp
+++ tests/CompileTimeRandomTest.cpp
@@ -1,4 +1,12 @@
+#include <string>
+
 #include "doctest.h"
+
+template<int N>
+auto operator""_obfuscate(const char (&str)[N])
+{
+    return str;
+}
 
 int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
 
@@ -9,3 +17,9 @@ TEST_CASE("testing the factorial function") {
 		        CHECK(factorial(10) == 3628800);
 }
 
+TEST_CASE("Encrypt\\decrypt a string")
+{
+    auto encrypted = "Hello World"_obfuscate;
+    std::string strDecrypted = encrypted.Decrypt();
+    CHECK(strDecrypted == "Hello World");
+}
